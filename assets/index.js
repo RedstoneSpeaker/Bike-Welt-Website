d6a64ef811ebdac05ee557dd35deec3fc53adc38
RedstoneSpeaker <redstonespeaker@web.de>
2022-02-26 23:14:47 +0100

    merge

--- assets/index.js
+++ assets/index.js
@@ -1,11 +1,27 @@
 function gotoInformation(){
-    window.location.href = "information";
+    window.location.href = "../information";
 }
 
 function gotoEmployees(){
-    window.location.href = "employees";
+    window.location.href = "../employees";
 }
 
 function gotoContact(){
+    window.location.href = "../contact";
+}
+
+function gotoMain(){
+    window.location.href = "../"
+}
+
+function gotoInformationFromMain(){
+    window.location.href = "information";
+}
+
+function gotoEmployeesFromMain(){
+    window.location.href = "employees";
+}
+
+function gotoContactFromMain(){
     window.location.href = "contact";
 }
