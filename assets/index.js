b6c1c8b506c621ff3181ab750198528301c0785c
RedstoneSpeaker <redstonespeaker@web.de>
2022-02-26 20:19:45 +0100

    Main Page finished v1

---
+++ assets/index.js
@@ -0,0 +1,11 @@
+function gotoInformation(){
+    window.location.href = "information";
+}
+
+function gotoEmployees(){
+    window.location.href = "employees";
+}
+
+function gotoContact(){
+    window.location.href = "contact";
+}
